4e2d3dfcea1cdec3ec364481398467c12d7f2c99
MateusHBR <mateusmorais.r@gmail.com>
2020-06-25 14:46:50 -0300

    criando item reutilizavel

--- lib/modules/pages/home_page/components/item_opcao_widget.dart
+++ lib/modules/pages/home_page/components/item_opcao_widget.dart
@@ -1,15 +1,55 @@
 import 'package:flutter/material.dart';
 
 class ItemOpcaoWidget extends StatelessWidget {
+  final String title;
+  final IconData icon;
+
+  const ItemOpcaoWidget({Key key, @required this.title, @required this.icon})
+      : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
     return Container(
       height: size.height * 0.08,
       width: size.height * 0.115,
-      color: Colors.white,
+      decoration: BoxDecoration(
+        color: Color.fromRGBO(145, 64, 169, 1),
+        borderRadius: BorderRadius.circular(size.height * 0.005),
+      ),
       margin: EdgeInsets.only(
         right: 10,
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(
+              left: size.width * 0.004,
+              top: size.width * 0.004,
+            ),
+            child: Icon(
+              icon,
+              color: Colors.white,
+              size: size.height * 0.035,
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(
+              left: size.width * 0.01,
+              right: size.width * 0.01,
+              bottom: size.width * 0.008,
+            ),
+            child: Text(
+              title,
+              style: TextStyle(
+                fontSize: size.height * 0.0158,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
