9674c21c52ecf3e8b5b54d8356dab617add83e03
MateusHBR <mateusmorais.r@gmail.com>
2020-06-25 14:46:16 -0300

    deixando mais limpo o codigo

--- lib/modules/pages/home_page/home_page.dart
+++ lib/modules/pages/home_page/home_page.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
-import 'package:nu_roxinho/modules/pages/home_page/components/cabecalh_widget.dart';
 import 'package:nu_roxinho/modules/pages/home_page/components/home_options.dart';
-import 'package:nu_roxinho/modules/pages/home_page/components/item_menu_widget.dart';
+import 'package:nu_roxinho/modules/pages/home_page/components/menu_inside_widget.dart';
 
 import 'package:nu_roxinho/modules/pages/home_page/home_controller.dart';
 
@@ -115,80 +114,7 @@ class _HomePageState extends State<HomePage>
                   height: size.height * 0.8,
                   width: double.infinity,
                   color: Color.fromRGBO(129, 38, 157, 1),
-                  child: ListView(
-                    children: <Widget>[
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: <Widget>[
-                          Container(
-                            margin: EdgeInsets.only(
-                              bottom: 5,
-                            ),
-                            height: size.height * 0.13,
-                            width: size.height * 0.13,
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              image: DecorationImage(
-                                image: AssetImage('assets/images/qr.png'),
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(bottom: 10),
-                            child: CabecalhoWidget(),
-                          ),
-                          Divider(
-                            color: Colors.white,
-                          ),
-                          ItemMenuWidget(
-                            icon: Icons.help_outline,
-                            title: 'Me ajuda',
-                            align: -1,
-                          ),
-                          ItemMenuWidget(
-                            icon: Icons.person_outline,
-                            title: 'Perfil',
-                            align: -1.0,
-                          ),
-                          ItemMenuWidget(
-                            icon: Icons.settings_applications,
-                            title: 'Configurar conta',
-                            align: -1.0,
-                          ),
-                          ItemMenuWidget(
-                            icon: Icons.credit_card,
-                            title: 'Configurar Cartão',
-                            align: -1.0,
-                          ),
-                          ItemMenuWidget(
-                            icon: Icons.phone_iphone,
-                            title: 'Configurações do app',
-                            align: -1.0,
-                          ),
-                          Container(
-                            margin: EdgeInsets.only(top: 10),
-                            width: double.infinity,
-                            height: size.height * 0.07,
-                            decoration: BoxDecoration(
-                              color: Colors.transparent,
-                              border: Border.all(color: Colors.white),
-                            ),
-                            alignment: Alignment.center,
-                            child: Text(
-                              'SAIR DA CONTA',
-                              style: TextStyle(
-                                color: Colors.white,
-                                letterSpacing: 0.5,
-                                fontWeight: FontWeight.bold,
-                                fontSize: size.height * 0.02,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
+                  child: MenuInside(),
                 ),
                 Transform(
                   transform: Matrix4.translationValues(
